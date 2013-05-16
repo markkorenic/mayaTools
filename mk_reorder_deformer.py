2b84970a57a256de3ea8a3089c694bcb9e08d789
markkorenic <korenic.mark@gmail.com>
2013-05-15 19:37:47 -0600

    added reorder buttons

--- mk_reorder_deformer.py
+++ mk_reorder_deformer.py
@@ -1,9 +1,13 @@
 """
-author: mark korenic
+author: mark korenic    skeletech.net
 file: mk_reorder_deformer.py
 created: April 21,2013
+
+SYNOPSIS: Reorder deformer inputs when 'All Inputs' fail to load.
 """
+
 import maya.cmds as cmds
+from functools import partial
 
 class mk_ReorderDeformers():
     def __init__(self):
@@ -23,89 +27,93 @@ class mk_ReorderDeformers():
         
         #create a window
         self.UI = cmds.window(self.windowName, width=self.windowWidth, height=self.windowHeight,
-            title="mk_reorder", sizeable=True, mxb=False, mnb=False)
+            title="mk_ReorderDeformer", sizeable=False, mxb=False, mnb=False)
 
         #create root layout
         self.mainLayout = cmds.columnLayout(rs = 5, bgc=[0.2, 0.2, 0.2])
     
         #influence list
-        self.scrollDeformerList = cmds.textScrollList( numberOfRows=8, w=self.windowWidth, h=140, bgc=[.4, .4, .4], ra =True,append=self.deformers, p=self.mainLayout)
+        self.scrollDeformerList = cmds.textScrollList( numberOfRows=8, w=350, h=140, bgc=[.4, .4, .4], ra =True,append=self.deformers, p=self.mainLayout)
         
         #create buttons
-        self.inputBtn = cmds.button(label='list inputs', width=380, height=30, enable=True,
+        self.inputBtn = cmds.button(label='List Deformer Inputs', width=350, height=30, enable=True,
         annotation='',p=self.mainLayout, command = self.popDeformerList)
-        self.moveUpBtn = cmds.button(label='Move Up', width=380, height=30, enable=True,
-        annotation='',p=self.mainLayout, command = self.moveItem)
-        self.moveDwnBtn = cmds.button(label='Move Down', width=380, height=30, enable=True,
-        annotation='',p=self.mainLayout, command = self.popDeformerList)
+        ### I adjusted the command arguments see imports at the top(functools) ###
+        self.moveUpBtn = cmds.button(label='Move Up', width=350, height=30, enable=True,\
+                                     annotation='',p=self.mainLayout, command = partial(self.moveItem,"up"))
+        self.moveDwnBtn = cmds.button(label='Move Down', width=350, height=30, enable=True,\
+                                      annotation='',p=self.mainLayout,command = partial(self.moveItem,"down"))
         
         #show window
         cmds.showWindow(self.windowName)
 
     def popDeformerList(self, selection, *args):
-        selection = self.getSelectedMesh()
-       
+        """populate inputs to scroll list from selected mesh"""        
+        selection = self.getSelectedMesh()       
         if selection == False:
-            cmds.headsUpMessage("Select Geometry")     
-         
+            cmds.headsUpMessage("Select Geometry")
          # Get the list of verts   
         deformerList = self.listDeformers(selection)
         print self.listDeformers()
         
-        
     def getSelectedMesh(self, *args):
         # Identify the selection
         selMesh = cmds.ls(sl=True, et='transform')
-        if selMesh == []:
-            return selMesh[0]
+        if selMesh:
+            return selMesh[0]        
     
     def listDeformers(self, *args):
-        self.getSelectedMesh()
-        self.deformers = []
-        listHistory = cmds.listHistory()
-        for node in listHistory:
-            nodeTypes = cmds.nodeType(node, inherited = True)
-            if 'geometryFilter' in nodeTypes:       
-                self.deformers.append(node)
-        #scroll list needed here to populate list
-        cmds.textScrollList(self.scrollDeformerList, edit=True,removeAll = True, append =self.deformers)
-    
-    def deformerReorder(self, *args):
-        #cmds.reorderDeformers()
-        pass
-    
-    def moveItem(self, direction = 'up', *args):
-        """moves deformer up in list"""
-        
-        original_list = cmds.textScrollList(self.scrollDeformerList, query=True,ai = True)
-        selected_item = cmds.textScrollList(self.scrollDeformerList, query=True, si= True)
-        
-        indexNum = original_list.index(selected_item[0])
-        print selected_item, indexNum
-        if direction == 'up':
-            previousItem = original_list[indexNum -1]
-            print previousItem
-            cmds.reorderDeformers(previousItem, selected_item, self.getSelectedMesh)
-            print self.deformers
+        """get deformer inputs from selected mesh"""
+        ### Make sure a mesh is selected
+        mesh = self.getSelectedMesh()
+        if mesh:
+            self.deformers = []
+            ### pass name to listHistory()
+            history = cmds.listHistory(mesh)
+            for node in history:
+                nodeTypes = cmds.nodeType(node, inherited = True)
+                if 'geometryFilter' in nodeTypes:       
+                    self.deformers.append(node)
+            #scroll list needed here to populate list
             cmds.textScrollList(self.scrollDeformerList, edit=True,removeAll = True, append =self.deformers)
-        
-        
-        
-    
-    #def dropItem(self, dragControl, dropControl, messages, x, y, dragType):
-         #print dragControl
-         #print dropControl
-         #print messages
-         #print x, ",", y
-         #print dragType
-         
-         
-    #def dragItem(self, dragControl, x, y, modifiers):
-        
-        #print dragControl
-        #print x, ",", y
-        #print modifiers
-        #return ['0']
-        
-            
+        else:
+            cmds.headsUpMessage("Please select an object.")
+   
+    def moveItem(self,move="up", *args):
+        """moves deformer up in list"""
+        ### Get mesh name
+        mesh = self.getSelectedMesh()
+        cmds.select(mesh,r=True)
+        ### Get shape node
+        cmds.pickWalk(direction="down")
+        meshShape = cmds.ls(sl=True)[0]
+        ### Reselect transform node
+        cmds.select(mesh,r=True)        
+        current_state = cmds.textScrollList(self.scrollDeformerList, query=True,ai = True)
+        selected_item = cmds.textScrollList(self.scrollDeformerList, query=True,si= True)[0]             
+        indexNum = current_state.index(selected_item)
+        # Move item UP
+        if move == 'up':
+            ### Dont move item if it is at the top of the list
+            if indexNum > 0:
+                # Get item ABOVE selected 
+                previousItem = current_state[indexNum-1]            
+                cmds.reorderDeformers(selected_item,previousItem,meshShape)
+                ### Update scroll list
+                self.listDeformers()
+                ### Re-select item
+                cmds.textScrollList(self.scrollDeformerList,edit=True,si= selected_item)
+        # Move item DOWN
+        else:
+            # Get item BELOW selected 
+            lastIndexNum = (len(current_state)-1)
+            ### Dont move item if it is at the bottom of the list
+            if indexNum < lastIndexNum:
+                nextItem = current_state[indexNum+1]            
+                cmds.reorderDeformers(nextItem,selected_item,meshShape)
+                ### Update scroll list
+                self.listDeformers()
+                ### Re-select item
+                cmds.textScrollList(self.scrollDeformerList,edit=True,si= selected_item) 
+
 mk_ReorderDeformers()
