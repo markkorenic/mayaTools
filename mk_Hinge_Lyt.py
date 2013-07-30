a0c2dde0b1bd5f597382a6ed205008ce93c4ba2a
Mark Korenic <korenic.mark@gmail.com>
2013-07-29 17:52:58 -0700

    rewritten in pymel

--- mk_Hinge_Lyt.py
+++ mk_Hinge_Lyt.py
@@ -1,28 +1,23 @@
-def createLegGuide():
-    """creates locator guides for leg joints"""
+import pymel.core as pm
+
+def createLegGuides():
+    """creates locator guides for leg"""
+    locators = []
+    #create leg guides, set initial positions
+    hipLoc = pm.spaceLocator(n = 'locator_1')
+    hipLoc.setPosition([2,9,4])
+   
+    kneeLoc = pm.spaceLocator(n = 'locator_2')
+    kneeLoc.setPosition([2, 6 ,6])
     
-    #create hip locator
-    hipLoc = cmds.spaceLocator( p=(3,15,0), n = 'locator_1' )
-    #check to see if hip was created, then center its pivot with xform command
-    for item in hipLoc:
-        cmds.xform( item, cp = True )
-        print 'hip guide created'
-    if not item:
-        print item + 'failed'
+    ankleLoc = pm.spaceLocator(n = 'locator_3')
+    ankleLoc.setPosition([2, 3 ,4])
+    #parent leg guides
+    ankleLoc.setParent(kneeLoc, hipLoc)
+    for each in hipLoc, kneeLoc, ankleLoc:
+        print each
+    #append locators to group
+    locators.append([hipLoc, kneeLoc,ankleLoc])
+    print locators
     
-    #create knee locator
-    kneeLoc = cmds.spaceLocator( p=(3, 7, 2), n = 'locator_2' )
-    for item in kneeLoc:
-        cmds.xform( item, cp = True ) 
-        print 'knee guide created'
-    if not item:
-        print item + 'failed'
-    
-    #create ankle locator
-    ankleLoc= cmds.spaceLocator( p=(3, 1, -1), n = 'locator_3' )
-    for item in ankleLoc:
-        cmds.xform( item, cp = True )
-        print 'ankle guide created'
-    if not item:
-        print item + 'failed'
-createLegGuide()
+createLegGuides()
