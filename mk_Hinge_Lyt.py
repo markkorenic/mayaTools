37990658ee25298dce84cbf9923be08ff8f36995
Mark Korenic <korenic.mark@gmail.com>
2013-07-30 21:56:27 -0700

    commented function

--- mk_Hinge_Lyt.py
+++ mk_Hinge_Lyt.py
@@ -1,23 +1,32 @@
 import pymel.core as pm
 
+#make  global list of locator names and positions
+locatorInfo = (['locator_1', [2,9,4]], ['locator_2', [2, 6 ,6]], ['locator_3', [2, 3 ,4]])
+
 def createLegGuides():
+
     """creates locator guides for leg"""
-    locators = []
+
+    locators = [] #create empty list
+
     #create leg guides, set initial positions
-    hipLoc = pm.spaceLocator(n = 'locator_1')
-    hipLoc.setPosition([2,9,4])
-   
-    kneeLoc = pm.spaceLocator(n = 'locator_2')
-    kneeLoc.setPosition([2, 6 ,6])
-    
-    ankleLoc = pm.spaceLocator(n = 'locator_3')
-    ankleLoc.setPosition([2, 3 ,4])
-    #parent leg guides
-    ankleLoc.setParent(kneeLoc, hipLoc)
-    for each in hipLoc, kneeLoc, ankleLoc:
-        print each
-    #append locators to group
-    locators.append([hipLoc, kneeLoc,ankleLoc])
+
+    for i in range(len(locatorInfo)):
+
+        print locatorInfo[i] #print list contents
+
+        loc = pm.spaceLocator(n = locatorInfo[i][0]) # create locators, rename
+
+        loc.setPosition(locatorInfo[i][1]) # set positions with locatorInfo positions[1]
+
+        if i != 0: # Use a condition to make sure this is not the root locator_1
+
+            loc.setParent(locatorInfo[i][0], locatorInfo[i-1][0]) #parent locators
+
+        #append locators to list
+
+        locators.append(loc)
+
     print locators
-    
+
 createLegGuides()
