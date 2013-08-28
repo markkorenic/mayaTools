c68a6601e9e811169c381b99548bfd854b5d6ebf
Mark Korenic <korenic.mark@gmail.com>
2013-08-27 18:30:22 -0700

    created more accurate names for guides

--- Maya/Modules/Layout/mk_Leg_Lyt.py
+++ Maya/Modules/Layout/mk_Leg_Lyt.py
@@ -7,31 +7,30 @@ CLASS_NAME = 'Leg_Lyt'
 TITLE = 'Leg_LYT'
 DESCRIPTION = 'Builds a leg layout'
 
-locatorInfo = (['locator_1', [2,9,4]], ['locator_2', [2, 6 ,6]], ['locator_3', [2, 3 ,4]])
-
 class Leg_Lyt:
+
     def __init__(self):
         print "In Hinge Lyt"
-        #self.Leg_Lyt()
-
+        self.leg_lyt()
+        
     def leg_lyt(self,*args):
 
         """creates locator guides for leg"""
-
+        locatorInfo = (['root_locator', [2,9,4]], ['knee_locator', [2, 6 ,6]], ['ankle_locator', [2, 3 ,4]])
         locators = []
         #create leg guides, set initial positions
         for i in range(len(locatorInfo)):
-            print locatorInfo[i]
-
-            loc = pm.spaceLocator(n = locatorInfo[i][0])
-            loc.setPosition(locatorInfo[i][1])
-
-            if i != 0: # Use a condition to make sure this is not the root
+                print locatorInfo[i]
+        
+                loc = pm.spaceLocator(n = locatorInfo[i][0])
+                loc.setPosition(locatorInfo[i][1])
+        
+        if i != 0: # Use a condition to make sure this is not the root
                 loc.setParent(locatorInfo[i][0], locatorInfo[i-1][0]) #parent locator_3 to locator_1
-
-            #append locators to list
-            locators.append(loc)
-
+        
+        #append locators to list
+        locators.append(loc)
+        
         print locators
 
 Leg_Lyt()
