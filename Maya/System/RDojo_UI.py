097f7fa131916dd6b9961c1a61c49527730e2891
Mark Korenic <korenic.mark@gmail.com>
2013-08-27 18:31:26 -0700

    created checkBox for FKIK functionality

--- Maya/System/RDojo_UI.py
+++ Maya/System/RDojo_UI.py
@@ -3,21 +3,20 @@ author: Mark Korenic   www.skeletech.net
 file: RDojo_UI.py
 """
 import pymel.core as pm
-import Maya.Modules.Layout.mk_Leg_Lyt as Leg_Lyt
-reload(Leg_Lyt)
+
 
 class Rdojo_UI:
  """UI for creating rig parts"""
 
  def __init__(self):
   print "In Rdojo_UI"
-
+  
   #Create a dictionary to store UI elements
   self.UIElements = {}
   
   #Check to see if the UI exists
   #more pymelly
-  winName = self.windowName = "RDojo_UI"
+  winName = self.windowName = "RD_UI"
   if pm.objExists(winName):
       winName.deleteUI()
   
@@ -25,30 +24,35 @@ class Rdojo_UI:
   self.windowHeight = 100
   self.buttonWidth = 70
   self.buttonHeight = 33
-  
   #create window
-  windowObject= self.UIElements["window"] = pm.window(winName, width=self.windowWidth, height=self.windowHeight, title="RDojo_UI", sizeable=True)
+  windowObject= self.UIElements["window"] = pm.window(winName, width=self.windowWidth, height=self.windowHeight, title="RDUI", sizeable=True)
   #Use a flow layout for the  UI
   self.UIElements["guiFlowLayout"] = pm.flowLayout(v=True, width=self.windowWidth, height=self.windowHeight, bgc=[0.4, 0.4, 0.4])
 
-  self.UIElements["hingeButton"] = pm.button( label='Hinge_Lyt', width=self.buttonWidth, h=self.buttonHeight, c = self.createLegLyt)
-  self.UIElements["RigLegButton"] = pm.button( label='RigLeg', width=self.buttonWidth,h=self.buttonHeight,c = self.rig_leg)
+  self.UIElements["hingeButton"] = pm.button( label='Hinge_Lyt', width=self.buttonWidth, h=self.buttonHeight, c = self.createLegLyt,p = self.UIElements["guiFlowLayout"])
+  self.chkBx=self.UIElements["fkikChkBx"] = pm.checkBox(label = 'FKIK', value = True,annotation = 'sets FKIK mode', p =self.UIElements["guiFlowLayout"],cc = self.fk_ik)
+  self.UIElements["RigLegButton"] = pm.button( label='RigLeg', width=self.buttonWidth,h=self.buttonHeight,c = self.rig_leg,p = self.UIElements["guiFlowLayout"])
   #Show the window
   windowObject.show()
-Rdojo_UI()
-        
-def createLegLyt(self, *args):
-        """creates joint layout"""
-        Leg_Lyt.leg_lyt()
-        Leg_Lyt.DESCRIPTION()
-        pm.button(self.UIElements["lytButton"], en = False)
+  
+ def createLegLyt(self, *args):
+  """creates joint layout"""
+  import Maya.Modules.Layout.mk_Leg_Lyt as Leg_Lyt
+  reload(Leg_Lyt)
+  Leg_Lyt.Leg_Lyt()
+         
+ def rig_leg(self, *args):
+  """places joints in layout positions"""
+  import Maya.Modules.Animation.mk_Rig_Leg as leg
+  reload(leg)
+  leg.Rig_Leg()
 
-def rig_leg(self, *args):
-    """places joints in layout positions"""
-    import mk_rig_leg as leg
-    reload(leg)
-    Rig_Leg.DESCRIPTION()
-    leg.Rig_Leg()
-    pm.button(self.UIElements['rigLegBtn'], en = False)
-    
+ def fk_ik(self, *args):
+     
+     if self.chkBx.getValue():
+         print 'in FKIK'
+     else:
+         print 'FKIK off'
+        
+
 Rdojo_UI()
