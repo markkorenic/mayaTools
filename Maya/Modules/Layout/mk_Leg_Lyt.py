383de0213d9c819d058e421cead454d7012ee4bb
Mark Korenic <korenic.mark@gmail.com>
2013-09-13 12:23:32 -0700

    Update mk_Leg_Lyt.py

--- Maya/Modules/Layout/mk_Leg_Lyt.py
+++ Maya/Modules/Layout/mk_Leg_Lyt.py
@@ -14,23 +14,21 @@ class Leg_Lyt:
         self.leg_lyt()
         
     def leg_lyt(self,*args):
-
-        """creates locator guides for leg"""
-        locatorInfo = (['root_locator', [2,9,4]], ['knee_locator', [2, 6 ,6]], ['ankle_locator', [2, 3 ,4]])
         locators = []
+        """creates locator guides for leg"""
+        locatorInfo = (['Root', [2,9,4]], ['knee', [2, 6 ,6]], ['ankle', [2, 3 ,4]])
+        
         #create leg guides, set initial positions
-        for i in range(len(locatorInfo)):
-                print locatorInfo[i]
+        for item in range(len(locatorInfo)):
+                print locatorInfo[item]
         
-                loc = pm.spaceLocator(n = locatorInfo[i][0])
-                loc.setPosition(locatorInfo[i][1])
+                loc = pm.spaceLocator(n = locatorInfo[item][0],relative =True)
+                loc.setPosition(locatorInfo[item][1])
         
-        if i != 0: # Use a condition to make sure this is not the root
-                loc.setParent(locatorInfo[i][0], locatorInfo[i-1][0]) #parent locator_3 to locator_1
+        if item != 0: # Use a condition to make sure this is not the root
+                loc.setParent(locatorInfo[item][0], locatorInfo[item-1][0]) #parent locator_3 to locator_1
         
         #append locators to list
         locators.append(loc)
         
         print locators
-
-Leg_Lyt()
