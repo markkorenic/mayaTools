47181ad5009e6d1fdc70926e8a555b097037d0fc
Mark Korenic <korenic.mark@gmail.com>
2013-10-29 13:48:24 -0700

    initial commit

--- mk_rigOrganizer.py
+++ mk_rigOrganizer.py
@@ -5,21 +5,13 @@ def rig_organizer():
 	'''creates group hierarchy for rig components'''
 	
 	#create groups
-	charGrp = pm.group(n = 'Character', w = True )
-	charGrp.deselect()
-	globMove = pm.group(n = 'globalMove_GRP')
-	globMove.deselect()
-	jntGrp = pm.group(n = "joints_GRP")
-	jntGrp.deselect()
-	geoGrp = pm.group(n = "geo_GRP")
-	geoGrp.deselect()
-	ikGrp = pm.group(n = "IK_GRP")
-	ikGrp.deselect()
-	ctrlGrp = pm.group(n = "ctrl_GRP")
-	ctrlGrp.deselect()
-	hideGrp= pm.group(n = "toHide_GRP")
-	hideGrp.deselect()
-	
+	charGrp = pm.group(n = 'Character', world = True,em=True )
+	globMove = pm.group(n = 'globalMove_GRP',em=True)
+	jntGrp = pm.group(n = "joints_GRP",em=True)
+	geoGrp = pm.group(n = "geo_GRP",em=True)
+	ikGrp = pm.group(n = "IK_GRP",em=True)
+	ctrlGrp = pm.group(n = "ctrl_GRP",em=True)
+	hideGrp= pm.group(n = "toHide_GRP", em=True)
 	#parent children 
 	charGrp.addChild(globMove)
 	charGrp.addChild(geoGrp)
@@ -33,8 +25,8 @@ def rig_organizer():
 		for ch in ["x", "y", "z"]:
 			charGrp.attr("%s%s"%(vec, ch)).lock()
 			globMove.attr("%s%s"%(vec, ch)).lock()
-			geoGrp.attr("%s%s"%(vec, ch)).lock()
+			geoGrp.	attr("%s%s"%(vec, ch)).lock()
 			jntGrp.attr("%s%s"%(vec, ch)).lock()
 			ikGrp.attr("%s%s"%(vec, ch)).lock()
 			hideGrp.attr("%s%s"%(vec, ch)).lock()
-rig_organizer()
+rig_organizer()		
